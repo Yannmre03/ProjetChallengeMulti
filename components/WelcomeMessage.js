c6a1a864d754a829df7878d22ff081072108608c
Yann Moore <yann.moore2003@yahoo.com>
2024-06-03 14:38:37 +0200

    test yann

---
+++ components/WelcomeMessage.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const WelcomeComponent = () => {
+    const [message, setMessage] = useState('');
+
+    const onButtonClick = () => {
+        setMessage('Bienvenue ');
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text>Bonjour</Text>
+            <Button title="Click" onPress={onButtonClick} />
+            {message !== '' && <Text>{message}</Text>}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+    }
+});
+
+export default WelcomeComponent;
